ea6c06be859c8787eaf9cb1048c3a8689531617e
yunqing <jzhang0703@163.com>
2025-11-21 15:42:42 +0800

    docs: 添加文章分类相关控制器上的详细说明

--- src/main/java/anthony/yublog/controller/CategoryController.java
+++ src/main/java/anthony/yublog/controller/CategoryController.java
@@ -17,7 +17,12 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 添加分类
+     * 功能1：
+     * 添加分类标签
+     * 添加新的文章标签，必须项：文章分类名称
+     * TODO: 文章分类别名允许用户输入
+     * TODO: 文章分类别名如果不输入，默系统默认创建。汉字别名创建为汉字的拼音首字母小写，如“测试”-“cs
+     * TODO: 英文别名默认为首个单词的前三个字母小写，如“travelNote”-“tra”
      */
     @PostMapping
     public Result<Object> add(@RequestBody @Validated Category category) {
@@ -25,8 +30,10 @@ public class CategoryController {
         return Result.success();
     }
 
-    /*
-        查询文章分类
+    /**
+     * 功能2：
+     * 查询文章分类标签
+     * 查询用户个人创建的所有文章分类标签
      */
     @GetMapping
     public Result<Object> list() {
@@ -34,8 +41,10 @@ public class CategoryController {
         return Result.success(cs);
     }
 
-    /*
-        查询文章分类详情
+    /**
+     * 功能3：
+     * 查询文章分类标签详情
+     * 根据文章分类标签id查询文章分类标签详情，两个字段：1. 分类标签名称 2. 分类标签别名
      */
     @GetMapping("/detail")
     public Result<Category> detail(Integer id) {
